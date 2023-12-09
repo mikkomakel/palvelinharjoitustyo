dccd86d0fb1cf95d13b93e6e25bca1aee477cb3c
Mikko <mikko.makela@seamk.fi>
2023-12-09 16:30:28 +0200

    Juuh elikkäs edellinen ohjelma ei alkanu toimimaan niin otetaan valmis ratkaisu jossa on joitain osia joita voidaan hyödyntää ja käyttäjien luominen

--- src/main/java/com/example/demo/MessageRepository.java
+++ src/main/java/com/example/demo/MessageRepository.java
@@ -2,5 +2,6 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
 }
