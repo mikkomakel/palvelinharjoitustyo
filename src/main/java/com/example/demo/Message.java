d96cfa54cd97250f009bf83675f9dc14b56a6046
Mikko <mikko.makela@seamk.fi>
2023-12-09 17:12:10 +0200

    Viestin mukana tulee nyt tieto kellonajasta kun se on luotu

--- src/main/java/com/example/demo/Message.java
+++ src/main/java/com/example/demo/Message.java
@@ -2,10 +2,15 @@ package com.example.demo;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import java.util.Date;
 
 @Entity
 @Data
@@ -16,5 +21,10 @@ public class Message extends AbstractPersistable<Long> {
     @ManyToOne
     private Account user;
     private String content;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
 
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
--- src/main/java/com/example/demo/MessageController.java
+++ src/main/java/com/example/demo/MessageController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Date;
+
 @Controller
 public class MessageController {
 
@@ -30,6 +32,8 @@ public class MessageController {
             Message msg = new Message();
             msg.setContent(content.trim());
 
+            msg.setCreatedAt(new Date());
+            
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             String username = auth.getName();
 
